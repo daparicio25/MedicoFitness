4b2c983d18bb90baab3c45e7fb8ad4080ead6c36
unknown <apariciodaniel250@gmail.com>
2019-04-25 11:19:55 -0500

    Initial commit

---
+++ src/app/config/config.ts
@@ -0,0 +1,11 @@
+export const Configuration = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAHBMrjL2FeNKT4lqZQjIh3Qoz-1B4BN84",
+    authDomain: "medicalfitness-50bb3.firebaseapp.com",
+    databaseURL: "https://medicalfitness-50bb3.firebaseio.com",
+    projectId: "medicalfitness-50bb3",
+    storageBucket: "medicalfitness-50bb3.appspot.com",
+    messagingSenderId: "829627893089"
+  }
+};
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+//FIREBASE
+import { AngularFirestoreModule } from "angularfire2/firestore";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+}
